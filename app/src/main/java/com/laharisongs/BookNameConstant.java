e0d90b7724937c582006a7bd2197e96363a81b68
Vishnu <vishnu160901@gmail.com>
2020-12-04 21:06:56 +0530

    Basic designs over.

---
+++ app/src/main/java/com/laharisongs/BookNameConstant.java
@@ -0,0 +1,8 @@
+package com.laharisongs;
+
+public class BookNameConstant {
+    public static final String[] TAMIL_BOOKS = {"தேவநீதியின் பாடல்கள்"};
+    public static final String[] TELUGU_BOOKS = {};
+    public static final String[] ENGLISH_BOOKS = {"Enlgish songs", "English chorus"};
+    public static final String[] HINDI_BOOKS = {};
+}
--- app/src/main/java/com/laharisongs/SongBooksListActivity.java
+++ app/src/main/java/com/laharisongs/SongBooksListActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class SongBooksListActivity extends AppCompatActivity {
+
+    public String[] books = new String[0];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,31 +19,33 @@ public class SongBooksListActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String language = intent.getStringExtra(BookConstant.LANGUAGE_SELECTED);
 
+        generateBooks(language);
+    }
+
+    protected void generateBooks(String language) {
+        LinearLayout booksIndex = (LinearLayout) findViewById(R.id.booksIndex);
+        Button book;
+
         switch (language) {
             case "TAMIL_SONG_BOOK" :
+                books = BookNameConstant.TAMIL_BOOKS;
                 break;
             case "TELUGU_SONG_BOOK" :
+                books = BookNameConstant.TELUGU_BOOKS;
                 break;
             case "ENGLISH_SONG_BOOK" :
+                books = BookNameConstant.ENGLISH_BOOKS;
                 break;
             case "HINDI_SONG_BOOK" :
+                books = BookNameConstant.HINDI_BOOKS;
                 break;
         }
-    }
 
-    protected void generateTamilBooks() {
-
-    }
-
-    protected void generateTeluguBooks() {
-
-    }
-
-    protected void generateEnglishBooks() {
-
-    }
-
-    protected void generateHindiBooks() {
-        
+        for(int i=0; i<books.length; i++) {
+            book = new Button(this);
+            book.setText(books[i]);
+            book.setId(i);
+            booksIndex.addView(book);
+        }
     }
 }
